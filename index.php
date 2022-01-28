2a2e3831cbf22f7352d1abf338c7a6fafeb85369
github-haruki <64785351+github-haruki@users.noreply.github.com>
2022-01-28 23:21:55 +0900

    newer

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $c26 = get_count('26');
 <body>
     <div class="header">
 		<div class="justbackground"></div>
-        <a href=""><img class="header_icon" src="logo.svg" alt="sai3 icon"></a>
+        <a href=""><img class="header_icon" src="file/logo.svg" alt="sai3 icon"></a>
 		<div class="hamburger">
                 <span></span>
                 <span></span>
@@ -68,85 +68,27 @@ $c26 = get_count('26');
             <img src="file\11.png" alt="">
             <h2>11HR</h2>
             <h1>まさかさかさま</h1>
-            <h3><?php echo $c11 ?></h3>
+            <h3><?php echo $c11 ?>分</h3>
         </div>
 		<div id="hr">
             <img src="file\11.png" alt="">
             <h2>12HR</h2>
             <h1>まさかさかさま</h1>
-            <h3><?php echo $c11 ?></h3>
+            <h3><?php echo $c12 ?>分</h3>
         </div>
 		<div id="hr">
             <img src="file\11.png" alt="">
             <h2>13HR</h2>
             <h1>まさかさかさま</h1>
-            <h3><?php echo $c11 ?></h3>
+            <h3><?php echo $c13 ?>分</h3>
         </div>
 		<div id="hr">
             <img src="file\11.png" alt="">
             <h2>14HR</h2>
             <h1>まさかさかさま</h1>
-            <h3><?php echo $c11 ?></h3>
+            <h3><?php echo $c14 ?>分</h3>
         </div>
     </div>
-
-
-	<script type="text/javascript">
-	function classify(){
-		var waiting11 = <?php echo $c11 ?> ; 
-		changeColor(waiting11, 11);
-
-		var waiting12 = <?php echo $c12 ?> ; 
-		changeColor(waiting12, 12);
-
-		var waiting13 = <?php echo $c13 ?> ; 
-		changeColor(waiting13, 13);
-
-		var waiting14 = <?php echo $c14 ?> ; 
-		changeColor(waiting14, 14);
-
-		var waiting15 = <?php echo $c15 ?> ; 
-		changeColor(waiting15, 15);
-
-		var waiting16 = <?php echo $c16 ?> ; 
-		changeColor(waiting16, 16);
-
-		var waiting17 = <?php echo $c17 ?> ; 
-		changeColor(waiting17, 17);
-
-		var waiting21 = <?php echo $c21 ?> ; 
-		changeColor(waiting21, 21);
-
-		var waiting22 = <?php echo $c22 ?> ; 
-		changeColor(waiting22, 22);
-
-		var waiting23 = <?php echo $c23 ?> ; 
-		changeColor(waiting23, 23);
-
-		var waiting24 = <?php echo $c24 ?> ; 
-		changeColor(waiting24, 24);
-
-		var waiting25 = <?php echo $c25 ?> ; 
-		changeColor(waiting25, 25);
-
-		var waiting26 = <?php echo $c26 ?> ; 
-		changeColor(waiting26, 26);
-		
-	}
-
-	function changeColor(waiting, hr){
-		let hr_id = 'cell' + hr ;
-		var obj = document.getElementById(hr_id); 
-		if(waiting == '準備完了'){
-    		obj.style.backgroundColor = '#ff0000b4'; 
-		}else if(waiting == '準備中'){
-			obj.style.backgroundColor = 'white'; 
-		}else{
-		}
-	}
-
-	window.onload = classify();
-	</script>
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script>
         $(function() {
